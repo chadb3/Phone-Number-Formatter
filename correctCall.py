8da47dd04951fcefe075bbbdbad797d6c75be2a3
chad <chadb78999@gmail.com>
2022-07-21 04:04:23 -0500

    Removed redundant list. Appends directly to the string now.

--- correctCall.py
+++ correctCall.py
@@ -2,20 +2,19 @@
 import re
 
 def translatePhone(phoneNumberIn):
-	retPhone =[]
-	retPhoneStr = "" 
+	#stores the updated phone number
+	retPhoneStr = ""
+	#Dictionary that holds each letter and its associated number for example pdict["A"]=2 and pdict["Z"]=9 
 	pdict = {"A":2,"B":2,"C":2,"D":3,"E":3,"F":3,"G":4,"H":4,"I":4,"J":5,"K":5,"L":5,"M":6,"N":6,"O":6,"P":7,"Q":7,"R":7,"S":7,"T":8,"U":8,"V":8,"W":9,"X":9,"Y":9,"Z":9}
 	#debug
 	#print(len(phoneNumberIn))
 	for i in range(0,len(phoneNumberIn)):
-		#print(phoneNumberIn[i],end="")
 		try:
-			retPhone.append(str(pdict[phoneNumberIn[i].upper()]))
+			#If possible, it converts the letter to number.
+			retPhoneStr+=str(pdict[phoneNumberIn[i].upper()])
 		except:
-			retPhone.append(phoneNumberIn[i])
-		retPhoneStr+=retPhone[i]
-	#odd behavior
-	#print("\nSTR: {}\nCorrected: {}".format(retPhoneStr, nullPhone(retPhoneStr)))
+			#otherwise it just adds it to the string.
+			retPhoneStr+=phoneNumberIn[i]
 	nullPhone(retPhoneStr)
 # Takes in a "Phone Number"
 def nullPhone(phoneNumberIn):
