82d5be3acaba6dbd9e40adaf33f09fcac00bbb69
Chad Bachman <chadb78999@gmail.com>
2022-07-26 00:04:54 -0500

    now gives a formatted phone number if one wasn't provided. Updated README

--- correctCall.py
+++ correctCall.py
@@ -1,7 +1,10 @@
-# needed for regex ( adding this comment as I didnt notice the date on this machine was wrong and I somehow now have commits on the 17th when this did not exist...)
+# needed for regex (Regular Expression)
 import re
-
+# Called First.
+# Removes Phonewords
+# then calls nullPhone(may later rename) to remove formatting for easy copy paste to software that doesn't like it. 
 def translatePhone(phoneNumberIn):
+	humanReadable(phoneNumberIn)
 	#stores if it the number was translated or not.
 	bool_Required_translation = False
 	#stores the updated phone number
@@ -20,26 +23,42 @@ def translatePhone(phoneNumberIn):
 			retPhoneStr+=phoneNumberIn[i]
 	if(bool_Required_translation):
 		print("Required Translation: {}".format(bool_Required_translation))
+		humanReadable(retPhoneStr)
 	nullPhone(retPhoneStr)
+	return 0
 # Takes in a "Phone Number"
+# Removes any formating from the phone number
+# Leaving only the digits.
 def nullPhone(phoneNumberIn):
 	# Prints the phone number in the before state
-	print("Before: {}".format(phoneNumberIn))
-
+	if(len(phoneNumberIn)>10):
+		print("Before: {}".format(phoneNumberIn))
 	# Debug Length from before regex
 	# print(len(phoneNumberIn))
-
 	# uses regex to match digits (the \D) and make everything else blank (the "")
 	newPhone = re.sub(r"\D", "", phoneNumberIn)
 	# prints only the digits (0-9)
 	# in other words, anything that wasn't a digit is destroyed.
 	print("\nAfter: {}\n".format(newPhone))
-
+	return 0
+# This will be responsible for printing a "Human Readable" phone number if one was not provided
+# if 5552345678 was entered, then it will print "Human Readable: 555-234-5678"
+# will also work with phone words. If "555ADGJMPT" was entered it will print "Human Readable: 555-ADG-JMPT"
+def humanReadable(phoneNumberIn):
+	humanReadbleNum=""
+	if(len(phoneNumberIn)==10):
+		for i in range(0,len(phoneNumberIn)):
+			#print(phoneNumberIn[i],end="")
+			humanReadbleNum+=phoneNumberIn[i]
+			if(i==2 or i==5):
+				#print("-",end="")
+				humanReadbleNum+="-"
+		print("Human Readable: {}\n".format(humanReadbleNum))
+	return 0
 # main calls the function currently called nullPhone (that removes everything that is not a digit).
 def main():
 	# now calls translate phone by default as it calls nullPhone as part of it.
 	translatePhone(input("\nEnter Phone Number: "))
 	return 0
-
 if __name__ == "__main__":
 	main()
