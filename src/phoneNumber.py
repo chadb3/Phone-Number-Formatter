c0ad6309abb2fc8f7f305e88cf25a021906a559d
Chad Bachman <chadb78999@gmail.com>
2022-07-27 15:47:40 -0500

    working on new branch to later merge

---
+++ src/phoneNumber.py
@@ -0,0 +1,11 @@
+class Phone_Number:
+    def getPhoneNumberParts(this, PhoneNumberIn):
+        print("CALLED")
+
+    def __init__(this,phoneNumberIn):
+        this.phone_number=phoneNumberIn
+        this.country_code=""
+        this.area_code=""
+        this.central_office_code=""
+        this.line_number=""
+        this.test()
