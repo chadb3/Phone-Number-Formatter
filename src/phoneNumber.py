6c3f3f7250a0a6ca88a1f8dffd1c2cb4d4720234
chad <chadb78999@gmail.com>
2022-07-28 00:00:53 -0500

    Continued work on phone_number class. More README corrections. added item to ignore.

--- src/phoneNumber.py
+++ src/phoneNumber.py
@@ -1,11 +1,25 @@
+import re
+# import like
+# from src import phoneNumber
 class Phone_Number:
-    def getPhoneNumberParts(this, PhoneNumberIn):
-        print("CALLED")
+	def translatePhone(this,phoneNumIn):
+		returnString = ""
+		pdict = {"A":2,"B":2,"C":2,"D":3,"E":3,"F":3,"G":4,"H":4,"I":4,"J":5,"K":5,"L":5,"M":6,"N":6,"O":6,"P":7,"Q":7,"R":7,"S":7,"T":8,"U":8,"V":8,"W":9,"X":9,"Y":9,"Z":9}
+		for i in range(0,len(phoneNumIn)):
+			try:
+				returnString+=str(pdict[phoneNumIn[i].upper()])
+			except:
+				returnString+=phoneNumIn[i]
+		print(returnString)
+	def getPhoneNumberParts(this, PhoneNumberIn):
+		print(len(PhoneNumberIn))
 
-    def __init__(this,phoneNumberIn):
-        this.phone_number=phoneNumberIn
-        this.country_code=""
-        this.area_code=""
-        this.central_office_code=""
-        this.line_number=""
-        this.test()
+	def __init__(this,phoneNumberIn):
+		this.phone_number_as_entered=phoneNumberIn
+		this.country_code=""
+		this.area_code=""
+		this.central_office_code=""
+		this.line_number=""
+		this.getPhoneNumberParts(phoneNumberIn)
+		this.translatePhone(phoneNumberIn)
+
