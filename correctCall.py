5522234e6b6e9c845c343625e2e8e88f6ef39cc0
chad <chad@pop-os.localdomain>
2022-08-07 01:46:57 -0500

    will not ask for a extension if input does not match a (us) phone number

--- correctCall.py
+++ correctCall.py
@@ -2,7 +2,8 @@ from src import *
 def main():
 	hasExtension=False
 	phone_number=Phone_Number(input("\nEnter Phone Number: "))
-	phone_number.addExtension(input("Extension (leave blank if no): "))
+	if(phone_number.isPhoneNumber):
+		phone_number.addExtension(input("Extension (leave blank if no): "))
 	phone_number.printPhoneNumber()
 	return 0
 if __name__ == "__main__":
