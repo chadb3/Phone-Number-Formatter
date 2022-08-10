579614b6763bf80ba0d62b06bc1dec24406504f2
chad <chadb78999@gmail.com>
2022-08-10 01:49:08 -0500

    added data source. added py program to get data out. added data to phoneNumber. Ran and did not get any errors

---
+++ data/excel_to_data.py
@@ -0,0 +1,19 @@
+import pylightxl as xl
+retStr = "{"
+dictionaryBase = "{}:\"{}\""
+db = xl.readxl('NpasInSvcByNumRpt.xlsx')
+sheet=db.ws_names[0]
+count=0
+skip=True
+#print(db.ws(sheet).row(row=2))
+for row in db.ws(sheet).rows:
+    if(not skip):
+        if(count>0):
+            retStr+=','
+        print(row)
+        retStr+=dictionaryBase.format(row[0],row[1])
+        count+=1
+    skip = False
+print("\n"*10)
+retStr+="}"
+print(retStr)
