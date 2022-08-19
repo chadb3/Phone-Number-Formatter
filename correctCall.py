bc33a94044ab1e1c5c63ba4cf8ce820ef5a48f2c
chad <chadb78999@gmail.com>
2022-08-19 01:01:14 -0500

    nightly upload

--- correctCall.py
+++ correctCall.py
@@ -5,6 +5,7 @@ def main():
 	if(phone_number.isPhoneNumber):
 		phone_number.addExtension(input("Extension (leave blank if no): "))
 	phone_number.printPhoneNumber()
+	phone_number.checkAreaCode()
 	return 0
 if __name__ == "__main__":
 	main()
