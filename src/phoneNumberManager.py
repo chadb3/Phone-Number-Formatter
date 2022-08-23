f00407b513f2f0148280a2ee99aed889d4bcf2e2
chad <chadb78999@gmail.com>
2022-08-23 00:47:00 -0500

    added phoneNumberManger

---
+++ src/phoneNumberManager.py
@@ -0,0 +1,15 @@
+from phoneNumber import *
+class phoneNumberManager():
+    def __init__(this):
+        this._PN_LIST=[]
+    def newPhoneNumber(this, pn_in):
+        pn=Phone_Number(pn_in)
+        this._PN_LIST.append(pn)
+    def newRandomPhoneNumber(this):
+        pn=Phone_Number()
+        this._PN_LIST.append(pn.generateRandom())
+    def debugTestPrintList(this):
+        #debug test print.
+        #may adjust to be actual at a later date.
+        for phone_number in this._PN_LIST:
+            print(phone_number)
