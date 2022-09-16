837d19cfd22550431f0156d976790d7879512959
chad <chadb78999@gmail.com>
2022-09-16 07:09:21 -0500

    working on db stuff

---
+++ src/phoneSQLHelper.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+def SELECT_PHONE_NUMBER(areaCodeIn):
+    con=sqlite3.connect("testDB.sqlite3")
+
+def SELECT_AREA_CODES_FROM_STATE(stateIN):
+    fileNamePath="data/PNDB.sqlite3"
+    sqlite3Connection=sqlite3.connect(fileNamePath)
+    cur=sqlite3Connection.cursor()
+
+def SELECT_STATE_FROM_ABBR(stateABBR):
+    fileNamePath="data/PNDB.sqlite3"
+
+
+def TEST_SELECT_STATE():
+    con=sqlite3.connect("testDB.sqlite3")
+    state=con.execute("SELECT STATE FROM US_STATES WHERE ID=111") #=None
+    print(state)
+    print(state.fetchone())
+    print("SELECT STATE FROM US_STATES WHERE ID=1")
+    try:
+        print("STATE: {}".format(str(state.fetchone()[0])))
+    except Exception as k:
+        print(k)
+    if(state.fetchone()==None):
+        print("NO DATA")
+    else:
+        print(state)
+    con.close()
